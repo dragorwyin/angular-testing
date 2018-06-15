0634c6c045c425c7d7fc603ebae28a21735d4f1b
Dawid Nowakowski <dn@idesign.center>
2018-06-15 09:33:50 +0200

    Add "messages" service and component to show messages about clicks and fetches

--- src/app/tour-of-heroes/tour-of-heroes.module.ts
+++ src/app/tour-of-heroes/tour-of-heroes.module.ts
@@ -5,13 +5,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './services/hero.service';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
   ],
-  declarations: [TourOfHeroesComponent, HeroesComponent, HeroDetailComponent],
+  declarations: [TourOfHeroesComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
   exports: [
     TourOfHeroesComponent
   ],
